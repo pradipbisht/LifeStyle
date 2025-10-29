b1cbc20d187c76238612d885a89bbb681214c177
pradipbisht <pradipbisht007@gmail.com>
2025-10-29 13:26:25 +0530

    second-envfix

--- firebase.ts
+++ firebase.ts
@@ -8,12 +8,12 @@ import { getStorage } from "firebase/storage";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAtyK9gc1qf2q19CR0LkSlQBvwOuQyVBoY",
-  authDomain: "e-com-36739.firebaseapp.com",
-  projectId: "e-com-36739",
-  storageBucket: "e-com-36739.appspot.com",
-  messagingSenderId: "814534951657",
-  appId: "1:814534951657:web:fb1c6c93e6d50c8010a678",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
